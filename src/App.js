7d5b5b21dc8cf4e4c6b2fad7f6a53976f13cd5cd
Cj9520 <jindalchirag7417@kgpian.iitkgp.ac.in>
2024-10-04 14:58:43 +0530

    0000

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { HashRouter as Router, Route, Router } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/home'
 import About from './components/About';
@@ -10,13 +10,13 @@ import Contact from './components/contact';
 function App() {
   return (
     <>
-      <Router>
+      <Routes>
         <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
         </Route>
-      </Router>
+      </Routes>
     </>
   );
 }
